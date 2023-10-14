b38673efafc0563642c3cac2e8ebe9396c0862d3
AntonZimyanin <zimyanin88@gmail.com>
2023-10-14 15:35:37 +0300

    Lab 1: Initial Codebase

---
+++ labNumber1/src/mainPackage/Food.java
@@ -0,0 +1,33 @@
+package mainPackage;
+
+public abstract class Food implements Consumable{
+    private String name;
+
+    public Food(String name){ 
+        this.name = name;
+    }
+
+    public String getName(){ 
+        return name;
+    }
+
+    public void setName(String name){ 
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){ 
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Food))
+            return false;
+        
+        if (name == null || ((Food)obj).name == null)
+            return false;
+        
+        return name.equals(((Food)obj).name);
+    }
+}
